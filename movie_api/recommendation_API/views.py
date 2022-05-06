e239d81c736be7ddef7d24cf311d33e30d1b111b
Kranthi Raju <67377577+kranthiraju@users.noreply.github.com>
2022-05-06 20:35:46 +0530

    filter , runtime, remove dups in tags-movies

--- movie_api/recommendation_API/views.py
+++ movie_api/recommendation_API/views.py
@@ -5,10 +5,10 @@ import pandas as pd
 from pathlib import Path
 
 root = Path('.')
-CBpath = root / 'data'/'CB_SimilarityMatrix.pkl'
-CFpath = root / 'data'/'CF_SimilarityMatrix.pkl'
-cb_df = pd.read_pickle(CBpath)
-collab_df = pd.read_pickle(CFpath)
+# CBpath = root / 'data'/'CB_SimilarityMatrix.pkl'
+# CFpath = root / 'data'/'CF_SimilarityMatrix.pkl'
+# cb_df = pd.read_pickle(CBpath)
+# collab_df = pd.read_pickle(CFpath)
 
 
 def getRelatedMovies(movieId, rating):
